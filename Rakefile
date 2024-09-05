12b32162c991dd726569911c13c8d310b9f4d006
Lewis Buckley <lewis@37signals.com>
2024-09-05 12:56:40 +0100

    Fix Multipart Post deprecation warning

--- Rakefile
+++ Rakefile
@@ -4,7 +4,6 @@ require "bundler/gem_tasks"
 require 'rake/testtask'
 Rake::TestTask.new do |test|
   test.libs << 'lib' << 'test'
-  test.ruby_opts << "-rubygems"
   test.pattern = 'test/**/*_test.rb'
   test.verbose = true
 end
--- lib/docusign_rest.rb
+++ lib/docusign_rest.rb
@@ -2,7 +2,6 @@ require_relative 'docusign_rest/version'
 require_relative 'docusign_rest/configuration'
 require_relative 'docusign_rest/client'
 require_relative 'docusign_rest/utility'
-require 'multipart_post' #require the multipart-post gem itself
 require 'net/http/post/multipart' #require the multipart-post net/http/post/multipart monkey patch
 require 'net/http'
 require 'json'
